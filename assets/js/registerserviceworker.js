2cb462ec47f53c0388cc38d06fd4771571ad55f6
Toya Okeke <60804873+toyaokeke@users.noreply.github.com>
2020-12-05 14:13:09 -0700

    Update 2020-12-05T21:10:44.965Z

--- assets/js/registerserviceworker.js
+++ assets/js/registerserviceworker.js
@@ -3,4 +3,4 @@
 * Template URL: https://github.com/toyaokeke/toyaokeke.github.io
 * Author: Toya Okeke
 */
-!function(){"use strict";"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("/service-worker.js")})}();
+!function(){"use strict";window.isUpdateAvailable=new Promise(function(o,n){"serviceWorker"in navigator&&-1===["localhost","192"].indexOf(location.hostname)&&navigator.serviceWorker.register("service-worker.js").then(function(e){e.onupdatefound=function(){var n=e.installing;n.onstatechange=function(){"installed"===n.state&&(navigator.serviceWorker.controller?o(!0):o(!1))}}}).catch(function(n){console.error("[SW ERROR]",n)})})}();
