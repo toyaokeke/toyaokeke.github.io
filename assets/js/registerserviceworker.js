c0660c54d044103c1e9fba315634b8fb08f80f6e
Toya Okeke <60804873+toyaokeke@users.noreply.github.com>
2020-12-10 23:07:07 -0700

    Update 2020-12-11T06:07:06.515Z

--- assets/js/registerserviceworker.js
+++ assets/js/registerserviceworker.js
@@ -3,4 +3,4 @@
 * Template URL: https://github.com/toyaokeke/toyaokeke.github.io
 * Author: Toya Okeke
 */
-!function(){"use strict";window.isUpdateAvailable=new Promise(function(o,e){"serviceWorker"in navigator&&-1===["localhost","192"].indexOf(location.hostname)&&navigator.serviceWorker.register("service-worker.js").then(function(t){t.onupdatefound=function(){var e=t.installing;e.onstatechange=function(){"installed"===e.state&&(navigator.serviceWorker.controller?o(!0):o(!1))}}}).catch(function(e){console.error("[SW ERROR]",e)})}),window.isUpdateAvailable.then(function(e){e&&this.toastCtrl.create({message:"New Update available! Reload to see the latest juicy changes.",position:"bottom",showCloseButton:!0}).present()})}();
+!function(){"use strict";window.isUpdateAvailable=new Promise(function(o,n){"serviceWorker"in navigator&&-1===["localhost","192"].indexOf(location.hostname)&&navigator.serviceWorker.register("service-worker.js").then(function(e){e.onupdatefound=function(){var n=e.installing;n.onstatechange=function(){"installed"===n.state&&(navigator.serviceWorker.controller?o(!0):o(!1))}}}).catch(function(n){console.error("[SW ERROR]",n)})})}();
